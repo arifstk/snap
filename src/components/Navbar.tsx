75d50d67b5204357dd26a83d107912b09919928e
arifstk <arifsk.dev@gmail.com>
2026-04-05 23:54:47 +0600

    navbar2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,14 @@
 // import React from 'react'
 'use client';
 
-import { SearchIcon, ShoppingCartIcon, User } from "lucide-react";
+import { LogOut, Package, SearchIcon, ShoppingCartIcon, User } from "lucide-react";
 import mongoose from "mongoose";
 import { AnimatePresence } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { motion } from "motion/react"
+import { signOut } from "next-auth/react";
 
 interface IUser {
   _id?: mongoose.Types.ObjectId;
@@ -46,7 +47,7 @@ const Navbar = ({ user }: { user: IUser }) => {
         </Link>
 
         <div className="relative">
-          <div className="bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform"
+          <div className="bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform cursor-pointer"
             onClick={() => setOpen(prev => !prev)}
           >
             {
@@ -64,6 +65,7 @@ const Navbar = ({ user }: { user: IUser }) => {
             }
           </div>
 
+          {/* dropdown */}
           <AnimatePresence>
             {open &&
               <motion.div
@@ -71,8 +73,44 @@ const Navbar = ({ user }: { user: IUser }) => {
                 animate={{ opacity: 1, y: 0, scale: 1 }}
                 transition={{ duration: 0.6 }}
                 exit={{ opacity: 0, y: -10, scale: 0.95 }}
-                className="absolute top-14 right-0 bg-white rounded-lg shadow-lg p-4 w-48"
+                className="absolute right-0 mt-3 bg-white rounded-lg shadow-xl border border-gray-200 p-2 w-48 z-999"
               >
+                <div className="flex items-center gap-3 py-2 border-b border-gray-200">
+                  <div className="w-10 h-10 rounded-full bg-green-100 flex items-center justify-center overflow-hidden relative">
+                    {
+                      user.image ? <Image src={user.image} alt='user' fill
+                        className="object-cover rounded-full w-10 h-10"
+                      /> :
+                        (<span className="text-green-700 font-bold text-2xl">
+                          {user.name
+                            .split(" ")
+                            .map(n => n[0])
+                            .join("")
+                            .slice(0, 2)
+                            .toUpperCase()}
+                        </span>)
+                    }
+                  </div>
+
+                  <div>
+                    <div className="text-gray-800 font-semibold tracking-tight">{user.name}</div>
+                    <div className="text-xs text-gray-500 capitalize">{user.role}</div>
+                  </div>
+                </div>
+
+                <Link href={""} className="flex items-center gap-2 mt-2 px-2 py-3 hover:bg-green-50 rounded-lg text-gray-700" onClick={() => setOpen(false)}>
+                  <Package className="w-5 h-5 text-green-600" />
+                  My Orders
+                </Link>
+
+                <button className="flex items-center gap-2 w-full text-left px-3 py-3 hover:bg-red-50 rounded-lg text-gray-700 font-medium cursor-pointer"
+                  onClick={() => {
+                    setOpen(false)
+                    signOut({ callbackUrl: "/login" })
+                  }}>
+                  <LogOut className="w-5 h-5 text-red-600" />
+                  Log Out
+                </button>
               </motion.div>
             }
           </AnimatePresence>
