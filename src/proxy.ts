883a5977215f1d4c8c5b4c556447eb6d270bcfb0
arifstk <arifsk.dev@gmail.com>
2026-04-06 21:59:41 +0600

    vercer bug fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,32 +5,33 @@ import { NextRequest, NextResponse } from "next/server";
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  const publicRoutes = [
-    "/login",
-    "/register",
-    "/api/auth",
-  ]
+  const publicRoutes = ["/login", "/register", "/api/auth"];
 
   if (publicRoutes.some((path) => pathname.startsWith(path))) {
     return NextResponse.next();
   }
 
-  const token = await getToken({ req, secret:process.env.AUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+    salt:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-authjs.session-token"
+        : "authjs.session-token",
+  });
   console.log(token);
   console.log(req.nextUrl);
 
   if (!token) {
-    const loginUrl = new URL("/login",req.url)
-    loginUrl.searchParams.set("callbackUrl", req.url)
-    return NextResponse.redirect(loginUrl)
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("callbackUrl", req.url);
+    return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-
-
-
