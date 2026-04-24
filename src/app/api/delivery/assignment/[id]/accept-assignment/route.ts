de6def170f83dcc558f750429af33f4e3cb2468b
arifstk <arifsk.dev@gmail.com>
2026-04-24 16:21:57 +0600

    Promise fix

--- src/app/api/delivery/assignment/[id]/accept-assignment/route.ts
+++ src/app/api/delivery/assignment/[id]/accept-assignment/route.ts
@@ -8,7 +8,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     await connectDb();
